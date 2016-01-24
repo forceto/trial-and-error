24df08cb8595ee8d9d9ff17bf9e5954b39378cfd
forceto <wangtong_1125@sina.com>
2016-01-24 09:59:04 +0800

    test

---
+++ src/chapter7/CalenderTest.java
@@ -0,0 +1,11 @@
+package chapter7;
+
+import java.util.Calendar;
+
+public class CalenderTest {
+	public CalenderTest(){
+		Calendar c=Calendar.getInstance();
+		
+		
+	}
+}
--- src/chapter7/StringBuilderTest.java
+++ src/chapter7/StringBuilderTest.java
@@ -4,14 +4,14 @@ public class StringBuilderTest {
 	public StringBuilderTest(){
 		StringBuilder s=new StringBuilder();
 		s.append("abc");
-		System.out.println(s);
+		System.out.println("×·¼Ó\t"+s);
 		s.insert(0, "000");
-		System.out.println(s);
+		System.out.println("²åÈë\t"+s);
 		s.replace(5, 6, ",");
-		System.out.println(s);
+		System.out.println("Ìæ»»\t"+s);
 		s.delete(5, 6);
-		System.out.println(s);
+		System.out.println("É¾³ı\t"+s);
 		s.reverse();
-		System.out.println(s);
+		System.out.println("µ¹ÖÃ\t"+s);
 	}
 }
