0776683c40bec258f8bf7ac1791e78e79357b012
forceto <wangtong_1125@sina.com>
2016-01-26 20:47:51 +0800

    new

--- src/chapter7/NewFormatterParse.java
+++ src/chapter7/NewFormatterParse.java
@@ -1,19 +1,22 @@
 package chapter7;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 public class NewFormatterParse {
 	public NewFormatterParse() {
-
+		newMethod();
 	}
 
 	private void selfTry() {
-		String s = "2000!!12!!23!! 23@12@12@";
-		DateTimeFormatter datef = DateTimeFormatter.ofPattern("yyyy!!MMM!!dd HH@mm@ss@");
+		String s = "2000!!12!!23 23@12@12@";
+		DateTimeFormatter datef = DateTimeFormatter.ofPattern(
+				"yyyy!!MM!!dd HH@mm@ss@");
 		LocalDateTime time = LocalDateTime.parse(s, datef);
 		System.out.println(time);
-
 	}
 
 	private void copyBook() {
@@ -32,5 +35,29 @@ public class NewFormatterParse {
 		DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy$$$MMM$$$dd HH小时");
 		LocalDateTime ld2 = LocalDateTime.parse(str2, dtf2);
 		System.out.println(ld2);
+	}
+	private void newMethod(){ 
+		// 取当前日期：
+		LocalDate today=LocalDate.now();
+		System.out.println(today);
+		// 根据年月日取日期，12月就是12：
+		LocalDate crischristmas = LocalDate.of(2014, 12, 25); // -> 2014-12-25
+		// 根据字符串取：
+		LocalDate endOfFeb = LocalDate.parse("2014-02-28"); // 严格按照ISO yyyy-MM-dd验证，02写成2都不行，当然也有一个重载方法允许自己定义格式
+		//LocalDate.parse("2014-02-29");  无效日期无法通过：DateTimeParseException: Invalid date
+
+		//日期转换经常遇到，比如：
+		// 取本月第1天：
+		LocalDate firstDayOfThisMonth = today.with(TemporalAdjusters.firstDayOfMonth()); // 2014-12-01
+		System.out.println("本月第一天："+firstDayOfThisMonth);
+		// 取本月第2天：
+		LocalDate secondDayOfThisMonth = today.withDayOfMonth(2); // 2014-12-02
+		// 取本月最后一天，再也不用计算是28，29，30还是31：
+		LocalDate lastDayOfThisMonth = today.with(TemporalAdjusters.lastDayOfMonth()); // 2014-12-31
+		// 取下一天：
+		LocalDate firstDayOf2015 = lastDayOfThisMonth.plusDays(1); // 变成了2015-01-01
+		// 取2015年1月第一个周一，这个计算用Calendar要死掉很多脑细胞：
+		LocalDate firstMondayOf2015 = LocalDate.parse("2015-01-01").
+				with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY)); // 2015-01-05
 	}
 }
