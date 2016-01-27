819e7d653b129805011c194926a1d6ad01f7f639
forceto <wangtong_1125@sina.com>
2016-01-27 15:21:57 +0800

    new

---
+++ src/chapter8/CollectionForEach.java
@@ -0,0 +1,17 @@
+package chapter8;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CollectionForEach {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public CollectionForEach(){
+		
+		Collection co=new HashSet();
+		co.add("a");
+		co.add("b");
+		co.add("c");
+		co.forEach(a->System.out.println(a));
+		
+	}
+}
