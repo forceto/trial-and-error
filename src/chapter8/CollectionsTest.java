6b4e45fda5ef2b895e927ebd3d1d855eb34df8bb
forceto <wangtong_1125@sina.com>
2016-02-14 12:14:11 +0800

    new

--- src/chapter8/CollectionsTest.java
+++ src/chapter8/CollectionsTest.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class CollectionsTest {
 	public CollectionsTest(){
-		unmodifiableTest();
+		listCollectionTest();
 	}
 	
 	@SuppressWarnings({"rawtypes","unchecked"})
