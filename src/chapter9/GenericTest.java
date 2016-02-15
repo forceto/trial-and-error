97fcc95d8b7445ec5d2eeef6683c759033495a55
forceto <wangtong_1125@sina.com>
2016-02-15 19:32:09 +0800

    new

--- src/chapter9/GenericTest.java
+++ src/chapter9/GenericTest.java
@@ -1,6 +1,7 @@
 package chapter9;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.Set;
 
 public class GenericTest {
 	public GenericTest(){
-		typeParameterTest();
+		useGenericMthod();
 	}
 	
 	//java 7 的菱形语法
@@ -84,9 +85,76 @@ public class GenericTest {
 //		String 不是Number的子类，下行报错
 //		AppleOfTypeParameter<String> a2=new AppleOfTypeParameter<>();
 	}
+	
+	//定义泛型方法，把array复制到Collection
+	public <T> void fromArrayToCollection(T[] a,Collection<T> c){
+		for(T o:a){
+			c.add((T) a);
+		}
+	}
+	
+	//使用泛型方法
+	public void genericMethodTest(){
+		Object[] ob=new Object[100];
+		for(int i=0;i<ob.length;i++){
+			ob[i]=(int)Math.random()*100;
+		}
+		
+		Collection<Object> co=new ArrayList<>();
+		//T-->Object
+		fromArrayToCollection(ob, co);
+		System.out.println(co);
+		
+		String [] sa=new String[10];
+		Collection<String> cs=new ArrayList<>();
+		//T-->String	sa-->String 	cs-->String
+		fromArrayToCollection(sa, cs);
+		
+		/*
+		 * --------以集合类的泛型为准----------
+		 * 以Collection中的泛型为最高上限，数组的类型必须是其或其子类
+		 */
+		//T-->Object	sa-->String 	co-->Object
+		//因为String为Object的子类所以可以
+		fromArrayToCollection(sa, co);
+		
+		Integer [] ia=new Integer[10];
+		Float [] fa=new Float[10];
+		Number [] na=new Number[10];
+		Collection<Number> cn=new ArrayList<>();
+		
+		//T-->Number	ia-->Integer	cn-->Number
+		fromArrayToCollection(ia, cn);
+		
+		//T-->Number	fa-->Float		cn-->Number 
+		fromArrayToCollection(fa, cn);
+		
+		//T-->Number 	na-->Number 	cn-->Number
+		fromArrayToCollection(na, cn);
+		
+		//T-->Object	na-->Number		co-->Object 
+		fromArrayToCollection(na, co);
 
+		//na-->Number 	cs-->String		Number不是String也不是其子类所以报错
+		//romArrayToCollection(na, cs); 
+	}
 	
+	private <T> void genericMethodTest2(List<? extends T> from,List<T> to){
+		for(T tem: from){
+			to.add(tem);
+		}
+	}
 	
+	private void useGenericMthod(){
+		List<Integer> apple=new ArrayList<>();
+		for(int i=0;i<5;i++){
+			apple.add(i*i);
+		}
+		List<Number> fruit=new ArrayList<>();
+		genericMethodTest2(apple, fruit);
+		System.out.println("apple:"+apple);
+		System.out.println("fruit:"+fruit);
+	}
 }
 
 
