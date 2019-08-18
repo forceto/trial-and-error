428a2c15f9ae1df412736285020ed028e72cdf97
wangtong <wangtong@wangtong>
2019-08-18 23:08:38 +0800

    revise

--- src/practiceJava/chapter_5/interfacePractice/Qiche.java
+++ src/practiceJava/chapter_5/interfacePractice/Qiche.java
@@ -2,7 +2,7 @@ package practiceJava.chapter_5.interfacePractice;
 /**
  * 
  * @author wangtong
- * @description java 中的一个接口只能继承接口，且可以继承多个其他接口
+ * @description java 中的接口只能继承接口，且可以继承多个其他接口
  */
 public interface Qiche extends Car, Vehicle {
 	public abstract void qiChePao();
